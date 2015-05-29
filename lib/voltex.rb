9d4878dde57ec14d960f534668a7145b1f07be68
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2015-05-29 12:27:14 -0500

    Moving setup require

--- lib/voltex.rb
+++ lib/voltex.rb
@@ -1,4 +1,7 @@
+require 'rails'
 require 'voltex/engine'
+require 'voltex/setup'
 
 module Voltex
+  extend Setup
 end
--- lib/voltex/engine.rb
+++ lib/voltex/engine.rb
@@ -1,8 +1,4 @@
-require 'voltex/setup'
-
 module Voltex
-  extend Setup
-
   class Engine < ::Rails::Engine
     isolate_namespace Voltex
   end
